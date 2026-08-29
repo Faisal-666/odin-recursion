d3f8cc7e21dd8aa49be53cea1517eda83b86f77e
Faisal-666 <Faisalakbar661@gmail.com>
2026-08-29 07:32:12 +0700

    change spread to indexing on merge

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -1,16 +1,18 @@
-const sort = (left, right) => {
+const merge = (left, right) => {
     const result = [];
 
     while(left.length || right.length) {
         if (left[0] < right[0]) {
-            result.push(...left.splice(0, 1));
+            result.push(left.splice(0, 1)[0]);
         } else if (left[0] > right[0]) {
-            result.push(...right.splice(0, 1));
+            result.push(right.splice(0, 1)[0]);
         } else if (left[0] === right[0]) {
-            result.push(...left.splice(0, 1));
-        } else if (!left.length || !right.length) {
-            result.push(...left.splice(0, left.length));
-            result.push(...right.splice(0, right.length));
+            result.push(left.splice(0, 1)[0]);
+        } else  {
+            !left.length 
+                ? result.push(...right.splice(0, right.length))
+                : result.push(...left.splice(0, left.length))
+            ;
         }
     }
 
@@ -24,7 +26,7 @@ const mergeSort = (arr) => {
     const left = arr.slice(0, midIndex);
     const right = arr.slice(midIndex, arr.length);
     
-    return sort(mergeSort(left), mergeSort(right));
+    return merge(mergeSort(left), mergeSort(right));
 };
 
 export default mergeSort;
